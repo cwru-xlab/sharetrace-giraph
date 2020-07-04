f64219f48e31139e73913a3bdbb63e563c6c4058
Ryan Tatton <rdt17@protonmail.com>
2020-07-04 10:36:33 -0400

    Adds constructor with input verification
    Checks against an empty collection of TemporalOccurrences and against two identical users.
    
    Signed-off-by: Ryan Tatton <rdt17@protonmail.com>

--- src/main/java/model/Contact.java
+++ src/main/java/model/Contact.java
@@ -4,25 +4,58 @@ import lombok.Builder;
 import lombok.NonNull;
 import lombok.Value;
 
+import java.util.Collection;
 import java.util.Objects;
 import java.util.SortedSet;
 
-@Value @Builder public class Contact<U1, U2> implements Comparable<Contact<?, ?>>
+/**
+ * An encounter between two users at one or more points in time.
+ *
+ * Two contacts are considered equal if their contacts are equal, regardless of the field they are assigned. That is,
+ * a {@code Contact} with {@link #firstUser} {@code u1} and {@link #secondUser} {@code u2} is considered equal to a
+ * different {@code Contact} with equal {@code u1} and {@code u2} except assigned to the opposite field.
+ *
+ * @param <U1> Type of identifier of the first user.
+ * @param <U2> Type of identifier of the second user.
+ */
+@Value public class Contact<U1, U2> implements Comparable<Contact<?, ?>>
 {
     @NonNull Identifiable<U1> firstUser;
     @NonNull Identifiable<U2> secondUser;
     @NonNull SortedSet<TemporalOccurrence> occurrences;
-    private static final String ILLEGAL_TEMPORAL_OCCURRENCE_MESSAGE = "Occurrence already exists.";
-    private static final String ILLEGAL_REMOVAL_MESSAGE = "A contact must contain at least one occurrence.";
+    private static final String NO_OCCURRENCES_MESSAGE = "A contact must contain at least one occurrence.";
+    private static final String IDENTICAL_USERS_MESSAGE = "A contact must contain two distinct users.";
 
-    public boolean containsUser(Identifiable<?> userID)
+    @Builder
+    private Contact(@NonNull Identifiable<U1> user1,
+                    @NonNull Identifiable<U2> user2,
+                    @NonNull SortedSet<TemporalOccurrence> timesInContact)
     {
-        return userID.equals(firstUser) || userID.equals(secondUser);
+        verifyUsers(user1, user2);
+        verifyOccurrences(timesInContact);
+        firstUser = user1;
+        secondUser = user2;
+        occurrences = timesInContact;
+    }
+
+    private void verifyUsers(Identifiable<U1> user1, Identifiable<U2> user2)
+    {
+        if (user1.equals(user2))
+        {
+            throw new IllegalArgumentException(IDENTICAL_USERS_MESSAGE);
+        }
+    }
+
+    private static void verifyOccurrences(Collection<TemporalOccurrence> timesInContact)
+    {
+        if (timesInContact.isEmpty())
+        {
+            throw new IllegalArgumentException(NO_OCCURRENCES_MESSAGE);
+        }
     }
 
     @Override
-    /* See https://hadoop.apache.org/docs/current/api/org/apache/hadoop/io
-    /WritableComparable.html*/ public int hashCode()
+    /* See https://hadoop.apache.org/docs/current/api/org/apache/hadoop/io/WritableComparable.html*/ public int hashCode()
     {
         return Objects.hash(firstUser, secondUser);
     }
