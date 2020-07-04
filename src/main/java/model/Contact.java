c346201567e5c9b258f8545330d25c3ee7424628
Ryan Tatton <rdt17@protonmail.com>
2020-07-04 10:14:27 -0400

    Implements, add(), addAll(), remove(), removeAll(), and equals()
    UserID<?> concrete class fields are replaced with Identifiable interface.
    Implements SortedSet<TemporalOccurrence> to allow for multiple occurrences of contact between the same two users.
    add(), addAll(), remove(), and removeAll() will be removed in a future commit, but are kept in the case they want to be re-implemented.
    equals() claims two Contact instances are equal if both users are the same, regardless of the order they are assigned.
    
    Signed-off-by: Ryan Tatton <rdt17@protonmail.com>

--- src/main/java/model/Contact.java
+++ src/main/java/model/Contact.java
@@ -4,34 +4,87 @@ import lombok.Builder;
 import lombok.NonNull;
 import lombok.Value;
 
-import java.time.Duration;
-import java.time.Instant;
+import java.util.Collection;
 import java.util.Objects;
+import java.util.SortedSet;
 
-@Value @Builder public class Contact implements Comparable<Contact>
+@Value @Builder public class Contact<U1, U2> implements Comparable<Contact<?, ?>>
 {
-    @NonNull UserID<?> firstUser;
-    @NonNull UserID<?> secondUser;
-    @NonNull Instant timeOfContact;
-    @NonNull Duration durationOfContact;
+    @NonNull Identifiable<U1> firstUser;
+    @NonNull Identifiable<U2> secondUser;
+    @NonNull SortedSet<TemporalOccurrence> occurrences;
+    private static final String ILLEGAL_TEMPORAL_OCCURRENCE_MESSAGE = "Occurrence already exists.";
+    private static final String ILLEGAL_REMOVAL_MESSAGE = "A contact must contain at least one occurrence.";
 
-    public boolean containsUser(UserID<?> userID)
+    public boolean containsUser(Identifiable<?> userID)
     {
-        return userID.equals(getFirstUser()) || userID.equals(getSecondUser());
+        return userID.equals(firstUser) || userID.equals(secondUser);
+    }
+
+    public boolean addAll(Collection<TemporalOccurrence> temporalOccurrences)
+    {
+        return occurrences.addAll(temporalOccurrences);
+    }
+
+    public boolean add(TemporalOccurrence occurrence) throws IllegalArgumentException
+    {
+        verifyAdd(occurrence);
+        return occurrences.add(occurrence);
+    }
+
+    private void verifyAdd(TemporalOccurrence occurrence) throws IllegalArgumentException
+    {
+        if (occurrences.contains(occurrence))
+        {
+            throw new IllegalArgumentException(ILLEGAL_TEMPORAL_OCCURRENCE_MESSAGE);
+        }
+    }
+
+    public boolean removeAll(Collection<TemporalOccurrence> occurrences)
+    {
+        return occurrences.removeAll(occurrences);
+    }
+
+    public boolean remove(TemporalOccurrence occurrence) throws IllegalStateException
+    {
+        verifyRemove(occurrence);
+        return occurrences.remove(occurrence);
+    }
+
+    private void verifyRemove(TemporalOccurrence occurrence) throws IllegalStateException
+    {
+        if (hasSingleOccurrence() && occurrences.contains(occurrence))
+        {
+            throw new IllegalStateException(ILLEGAL_REMOVAL_MESSAGE);
+        }
+    }
+
+    private boolean hasSingleOccurrence()
+    {
+        return 1 == occurrences.size();
     }
 
     @Override
     /* See https://hadoop.apache.org/docs/current/api/org/apache/hadoop/io
     /WritableComparable.html*/ public int hashCode()
     {
-        return Objects.hash(getFirstUser(),
-                            getSecondUser(),
-                            getTimeOfContact(),
-                            getDurationOfContact());
+        return Objects.hash(firstUser, secondUser);
     }
 
-    @Override public int compareTo(Contact o)
+    @Override public int compareTo(@NonNull Contact<?, ?> o)
     {
-        return getDurationOfContact().compareTo(o.getDurationOfContact());
+        return occurrences.first().compareTo(o.getOccurrences().first());
+    }
+
+    @Override public boolean equals(Object obj)
+    {
+        if (this == obj)
+            return true;
+        if (null == obj || getClass() != obj.getClass())
+            return false;
+        Contact<?, ?> contact = (Contact<?, ?>) obj;
+        boolean withSameOrder = firstUser.equals(contact.getFirstUser()) && secondUser.equals(contact.getSecondUser());
+        boolean withDiffOrder = firstUser.equals(contact.getSecondUser()) && secondUser.equals(contact.getFirstUser());
+        return withSameOrder || withDiffOrder;
     }
 }
