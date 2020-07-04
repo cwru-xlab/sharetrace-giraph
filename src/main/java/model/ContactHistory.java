ec34bd1a4921ea5b317dfd9c44e52362f6e2732a
Ryan Tatton <rdt17@protonmail.com>
2020-07-04 10:57:12 -0400

    Removes unused methods, adds Javadoc, and implements history as Set
    Signed-off-by: Ryan Tatton <rdt17@protonmail.com>

--- src/main/java/model/ContactHistory.java
+++ src/main/java/model/ContactHistory.java
@@ -1,32 +1,18 @@
 package main.java.model;
 
-import lombok.Builder;
 import lombok.NonNull;
 import lombok.Value;
 
-import java.time.Instant;
-import java.util.HashSet;
-import java.util.Map;
 import java.util.Set;
 
-@Value @Builder public class ContactHistory
+/**
+ * A grouping of {@link Contact}s that intended is intended to be for a the same user; however, no such restriction
+ * is applied at the time of instantiation.
+ *
+ * @param <U1> Type of identifier for the first user of {@link Contact}.
+ * @param <U2> Type of identifier for the second user of a {@link Contact}.
+ */
+@Value(staticConstructor = "of") public class ContactHistory<U1, U2>
 {
-    @NonNull Map<Instant, Set<Contact>> history;
-
-    public boolean contactedBy(UserID<?> userID)
-    {
-        return getContacts().stream().anyMatch(c -> c.containsUser(userID));
-    }
-
-    public Set<Instant> getTimesOfContact()
-    {
-        return getHistory().keySet();
-    }
-
-    public Set<Contact> getContacts()
-    {
-        Set<Contact> allContacts = new HashSet<>();
-        getHistory().values().forEach(allContacts::addAll);
-        return allContacts;
-    }
+    @NonNull Set<Contact<U1, U2>> history;
 }
