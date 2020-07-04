1f71bf0e30923194934a7c6e392e3687d7c8502c
Ryan Tatton <rdt17@protonmail.com>
2020-07-04 08:51:25 -0400

    Adds Identifiable<T> interface
    Attempt toward abstraction by using interfaces instead of concrete classes for fields
    
    Signed-off-by: Ryan Tatton <rdt17@protonmail.com>

---
+++ src/main/java/model/Identifiable.java
@@ -0,0 +1,10 @@
+package main.java.model;
+
+/**
+ * A property of anything used as an identifier.
+ * @param <T> Type of identifier.
+ */
+@FunctionalInterface public interface Identifiable<T>
+{
+    T getId();
+}
