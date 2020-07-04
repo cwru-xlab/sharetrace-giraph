e2e4f4784e5cd89fe2700c73ac2cffa991e41572
Ryan Tatton <rdt17@protonmail.com>
2020-07-04 09:32:24 -0400

    Implements ComputedValue<N> interface
    Some coding style changes as well, but nothing that affects functionality.
    
    Signed-off-by: Ryan Tatton <rdt17@protonmail.com>

--- src/main/java/model/RiskScore.java
+++ src/main/java/model/RiskScore.java
@@ -1,45 +1,48 @@
 package main.java.model;
 
-import lombok.Builder;
-import lombok.NonNull;
-import lombok.Value;
+import lombok.*;
 
 import java.io.IOException;
 
 /**
  * A value ranging between 0 and 1 that denotes the risk of some condition.
  *
- * The default implementation of compareTo(RiskScore<N>) uses the {@code double} value of the score.
+ * The default implementation of compareTo(RiskScore) uses the {@code double} value of the score.
  *
  * @param <N> Numerical type of the score.
  */
-@Value @Builder public class RiskScore<N extends Number> implements Comparable<RiskScore<N>>
+@Value public class RiskScore<N extends Number> implements Comparable<RiskScore<N>>, ComputedValue<N>
 {
-    @NonNull N score;
-    public static final String INVALID_RISK_SCORE_MESSAGE = "must be between 0" + " and 1, inclusive.";
+    @NonNull @Getter(AccessLevel.NONE) N score;
+    public static final String INVALID_RISK_SCORE_MESSAGE = " must be between 0 and 1, inclusive.";
 
-    @Builder public RiskScore(@NonNull N riskScore) throws IOException
+    @Builder private RiskScore(@NonNull N riskScore) throws IOException
     {
         verifyScore(riskScore);
-        this.score = riskScore;
+        score = riskScore;
     }
 
-    private void verifyScore(@NonNull N score) throws IOException
+    private void verifyScore(@NonNull N riskScore) throws IOException
     {
-        double risk = score.doubleValue();
-        if (risk <= 0 && 1 <= risk)
+        double risk = riskScore.doubleValue();
+        if (0.0 >= risk && 1.0 <= risk)
         {
             throw new IOException(formatInvalidRiskScoreMessage(risk));
         }
     }
 
-    private String formatInvalidRiskScoreMessage(Double score)
+    private static String formatInvalidRiskScoreMessage(Double score)
     {
-        return score.toString() + INVALID_RISK_SCORE_MESSAGE;
+        return score + INVALID_RISK_SCORE_MESSAGE;
     }
 
-    @Override public int compareTo(RiskScore<N> riskScore)
+    @Override public int compareTo(RiskScore<N> o)
     {
-        return Double.compare(getScore().doubleValue(), riskScore.getScore().doubleValue());
+        return Double.compare(score.doubleValue(), o.getValue().doubleValue());
+    }
+
+    @Override public N getValue()
+    {
+        return score;
     }
 }
