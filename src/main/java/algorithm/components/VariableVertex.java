ee58f83f97d8139943b7e09e4a2355ce8e4c8560
Ryan Tatton <rdt17@protonmail.com>
2020-07-05 09:59:05 -0400

    Deletes unused components

--- src/main/java/algorithm/components/VariableVertex.java
+++
@@ -1,9 +0,0 @@
-package main.java.algorithm.components;
-
-import org.apache.giraph.graph.DefaultVertex;
-import org.apache.hadoop.io.NullWritable;
-
-public class VariableVertex
-        extends DefaultVertex<UserRiskScoreWritableComparable,
-        UserRiskScoreWritable, NullWritable>
-{}
