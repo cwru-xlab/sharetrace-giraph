c62e847aa1c9dcc68f6b599caf06d1def82b8f15
Ryan Tatton <rdt17@protonmail.com>
2020-07-05 09:18:17 -0400

    Implements write(DataOutput) and read(DataInput); deprecated
    This implementation should only be used if wanting to store the entire Contact object in the ID, as opposed to partitioning ID information and occurrence information.
    
    Signed-off-by: Ryan Tatton <rdt17@protonmail.com>

--- src/main/java/algorithm/components/ContactWritableComparable.java
+++ src/main/java/algorithm/components/ContactWritableComparable.java
@@ -1,40 +1,70 @@
 package main.java.algorithm.components;
 
-import lombok.Data;
+import lombok.AccessLevel;
+import lombok.Getter;
 import lombok.NonNull;
+import lombok.Setter;
 import main.java.model.Contact;
+import main.java.model.Identifiable;
+import main.java.model.TemporalOccurrence;
+import main.java.model.UserId;
 import org.apache.hadoop.io.WritableComparable;
 
 import java.io.DataInput;
 import java.io.DataOutput;
 import java.io.IOException;
+import java.time.Duration;
+import java.time.Instant;
+import java.util.SortedSet;
+import java.util.TreeSet;
 
-@Data public class ContactWritableComparable
-        implements WritableComparable<ContactWritableComparable>
+@Deprecated
+@Getter(AccessLevel.PRIVATE)
+@Setter(AccessLevel.PRIVATE)
+public class ContactWritableComparable implements WritableComparable<ContactWritableComparable>
 {
-    @NonNull private Contact contact;
-
-    @Override public int compareTo(ContactWritableComparable o)
-    {
-        return getContact().compareTo(o.getContact());
-    }
+    @NonNull
+    private Contact<Long, Long> contact;
 
     @Override
-    // TODO Finalize output format
     public void write(DataOutput dataOutput) throws IOException
     {
-        dataOutput.writeUTF(getContact().toString());
+        dataOutput.writeLong(contact.getFirstUser().getId());
+        dataOutput.writeLong(contact.getSecondUser().getId());
+        int nOccurrences = contact.getOccurrences().size();
+        dataOutput.writeInt(nOccurrences);
+        for (TemporalOccurrence occurrence : contact.getOccurrences())
+        {
+            dataOutput.writeLong(occurrence.getTime().toEpochMilli());
+            dataOutput.writeLong(occurrence.getDuration().toMillis());
+        }
     }
 
     @Override
-    // TODO Finalize input format
     public void readFields(DataInput dataInput) throws IOException
     {
-        dataInput.readUTF();
+        Identifiable<Long> firstUser = UserId.of(dataInput.readLong());
+        Identifiable<Long> secondUser = UserId.of(dataInput.readLong());
+        int nOccurrences = dataInput.readInt();
+        SortedSet<TemporalOccurrence> occurrences = new TreeSet<>();
+        for (int iOccurrence = 0; iOccurrence < nOccurrences; iOccurrence++)
+        {
+            Instant time = Instant.ofEpochMilli(dataInput.readLong());
+            Duration duration = Duration.ofMillis(dataInput.readLong());
+            occurrences.add(TemporalOccurrence.of(time, duration));
+        }
+        setContact(Contact.of(firstUser, secondUser, occurrences));
     }
 
-    @Override public int hashCode()
+    @Override
+    public int compareTo(ContactWritableComparable o)
     {
-        return getContact().hashCode();
+        return contact.compareTo(o.getContact());
+    }
+
+    @Override
+    public int hashCode()
+    {
+        return contact.hashCode();
     }
 }
