b2120bc1589e24cb4c84cf67cd100db0f853d048
Ryan Tatton <rdt17@protonmail.com>
2020-07-06 07:33:55 -0400

    Generic extends Comparable; class does not implement ComputedValue
    To be able to access all the fields, the class no longer implements ComputedValue;
    but the riskScore field maintains this interface type.
    
    compareTo() first compares by risk score and then by updateTime if risk scores are
    equally comparable.
    
    Signed-off-by: Ryan Tatton <rdt17@protonmail.com>

--- src/main/java/model/TemporalRiskScore.java
+++ src/main/java/model/TemporalRiskScore.java
@@ -1,37 +1,35 @@
 package main.java.model;
 
-import lombok.AccessLevel;
-import lombok.Getter;
 import lombok.NonNull;
 import lombok.Value;
 
 import java.time.Instant;
 
 /**
- * A {@link RiskScore} with a time for when it was last updated. The value returned by {@link #getValue()} is the
- * risk score, not {@link #updateTime}.
+ * A {@link RiskScore} with a time for when it was last updated.
+ * <p>
+ * The default implementation of {@link #compareTo(TemporalRiskScore)} is to first compare {@link #riskScore}. If the
+ * risk scores are equally comparable based on the former, then {@link #updateTime} is then used for comparison.
  *
  * @param <N> Numerical type of the risk score.
  */
 @Value(staticConstructor = "of")
-public class TemporalRiskScore<N extends Number> implements ComputedValue<N>
+public class TemporalRiskScore<N extends Number & Comparable<N>> implements Comparable<TemporalRiskScore<N>>
 {
     @NonNull
     Instant updateTime;
 
     @NonNull
-    @Getter(AccessLevel.NONE)
     ComputedValue<N> riskScore;
 
     @Override
-    public N getValue()
+    public int compareTo(@NonNull TemporalRiskScore<N> o)
     {
-        return riskScore.getValue();
-    }
-
-    @Override
-    public int compareTo(@NonNull N o)
-    {
-        return riskScore.compareTo(o);
+        int compare = riskScore.compareTo(o.getRiskScore());
+        if (0 == compare)
+        {
+            compare = updateTime.compareTo(o.getUpdateTime());
+        }
+        return compare;
     }
 }
