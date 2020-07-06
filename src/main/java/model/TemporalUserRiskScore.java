edffa49ed752e35ac3a146d1ae64db55421c9524
Ryan Tatton <rdt17@protonmail.com>
2020-07-06 07:34:43 -0400

    Adds TemporalUserRiskScore
    Generic extends Comparable; class does not implement ComputedValue
    
    compareTo() first compares by risk score, then by updateTime if riskScores are
    equally comparable, and finally by userId if updateTimes are equally comparable.
    
    Signed-off-by: Ryan Tatton <rdt17@protonmail.com>

---
+++ src/main/java/model/TemporalUserRiskScore.java
@@ -0,0 +1,44 @@
+package main.java.model;
+
+import lombok.NonNull;
+import lombok.Value;
+
+import java.time.Instant;
+
+/**
+ * A {@link RiskScore} with a time for when it was last updated and an associated user.
+ * <p>
+ * The default implementation of {@link #compareTo(TemporalUserRiskScore)} is to first compare {@link #riskScore}. If
+ * the risk scores are equally comparable based on the former, {@link #updateTime} is then used for comparison.
+ * Finally, if the risk scores are still equally comparable based on the former two criteria, the {@link #userId} is
+ * used for comparison.
+ *
+ * @param <N> Numerical type of the risk score.
+ */
+@Value(staticConstructor = "of")
+public class TemporalUserRiskScore<U, N extends Number> implements Comparable<TemporalUserRiskScore<U, N>>
+{
+    @NonNull
+    Identifiable<U> userId;
+
+    @NonNull
+    Instant updateTime;
+
+    @NonNull
+    ComputedValue<N> riskScore;
+
+    @Override
+    public int compareTo(@NonNull TemporalUserRiskScore<U, N> o)
+    {
+        int compare = riskScore.compareTo(o.getRiskScore());
+        if (0 == compare)
+        {
+            compare = updateTime.compareTo(o.getUpdateTime());
+            if (0 == compare)
+            {
+                compare = userId.compareTo(o.getUserId());
+            }
+        }
+        return compare;
+    }
+}
