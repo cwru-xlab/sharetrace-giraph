5f3b307dc5893fc903d76ebf81b08af1143fa3f4
Ryan Tatton <rdt17@protonmail.com>
2020-07-07 08:06:57 -0400

    Cleans up first implementation of compute()
    Signed-off-by: Ryan Tatton <rdt17@protonmail.com>

--- src/main/java/algorithm/components/VariableVertexComputation.java
+++ src/main/java/algorithm/components/VariableVertexComputation.java
@@ -8,10 +8,9 @@ import org.apache.giraph.graph.Vertex;
 import org.apache.hadoop.io.NullWritable;
 
 import java.io.IOException;
-import java.util.Set;
-import java.util.SortedSet;
+import java.util.Collection;
+import java.util.NavigableSet;
 import java.util.TreeSet;
-// TODO Update Javadoc when types are finalized
 
 /**
  * Computation performed at every factor {@link Vertex} of the factor graph. The following are the elements that
@@ -20,7 +19,7 @@ import java.util.TreeSet;
  *     <li>{@link Vertex} ID: {@link Users}</li>
  *     <li>{@link Vertex} data: {@link RiskScoreData}</li>
  *     <li>{@link Edge} data: {@link NullWritable}</li>
- *     <li>Input message: {@link SortedRiskScores}</li>
+ *     <li>Input message: {@link RiskScoreData}</li>
  *     <li>Output message: {@link SortedRiskScores}</li>
  * </ul>
  * Each variable {@link Vertex} receives a single {@link TemporalUserRiskScore} from each of its factor vertices. After
@@ -28,25 +27,48 @@ import java.util.TreeSet;
  * vertices.
  */
 public class VariableVertexComputation
-        extends AbstractComputation<Users, SortedRiskScores, NullWritable, SortedRiskScores, SortedRiskScores>
+        extends AbstractComputation<Users, SortedRiskScores, NullWritable, RiskScoreData, SortedRiskScores>
 {
     // TODO Assumes all risk scores are kept in the vertex
-    // TODO Iterable<SortedRiskScores> might actually need to be Iterable<RiskScoreData>
     @Override
-    public void compute(Vertex<Users, SortedRiskScores, NullWritable> vertex, Iterable<SortedRiskScores> iterable)
+    public void compute(Vertex<Users, SortedRiskScores, NullWritable> vertex, Iterable<RiskScoreData> iterable)
             throws IOException
     {
-        // Combine all incoming risk scores with local risk scores
-        SortedSet<TemporalUserRiskScore<Long, Double>> allRiskScores = new TreeSet<>();
-        iterable.forEach(scores -> allRiskScores.addAll(scores.getSortedRiskScores()));
-        allRiskScores.addAll(vertex.getValue().getSortedRiskScores());
-        // Send all incoming risk scores to each factor vertex, except the one that came from that vertex
+        NavigableSet<TemporalUserRiskScore<Long, Double>> localRiskScores = vertex.getValue().getSortedRiskScores();
+        NavigableSet<TemporalUserRiskScore<Long, Double>> allRiskScores = combine(localRiskScores, iterable);
         for (TemporalUserRiskScore<Long, Double> score : allRiskScores)
         {
-            SortedSet<TemporalUserRiskScore<Long, Double>> outgoingRiskScores = new TreeSet<>(allRiskScores);
-            outgoingRiskScores.remove(score);
-            Set<Identifiable<Long>> user = Set.of(score.getUserId());
-            sendMessage(Users.of(user), SortedRiskScores.of(outgoingRiskScores));
+            Users receiver = finalizeReceiver(score.getUserId());
+            Identifiable<Long> sender = vertex.getId().getUsers().pollFirst();
+            SortedRiskScores outgoingMessages = finalizeOutgoing(sender, allRiskScores, score);
+            sendMessage(receiver, outgoingMessages);
         }
+    }
+
+    private static NavigableSet<TemporalUserRiskScore<Long, Double>> combine(
+            Collection<TemporalUserRiskScore<Long, Double>> localRiskScores,
+            Iterable<RiskScoreData> incomingRiskScores)
+    {
+        NavigableSet<TemporalUserRiskScore<Long, Double>> allRiskScores = new TreeSet<>();
+        incomingRiskScores.forEach(score -> allRiskScores.add(score.getRiskScore()));
+        allRiskScores.addAll(localRiskScores);
+        return allRiskScores;
+    }
+
+    private static SortedRiskScores finalizeOutgoing(
+            Identifiable<Long> sender,
+            NavigableSet<TemporalUserRiskScore<Long, Double>> allRiskScores,
+            TemporalUserRiskScore<Long, Double> receiver)
+    {
+        NavigableSet<TemporalUserRiskScore<Long, Double>> outgoing = new TreeSet<>(allRiskScores);
+        outgoing.remove(receiver);
+        return SortedRiskScores.of(sender, outgoing);
+    }
+
+    private static Users finalizeReceiver(Identifiable<Long> receiverId)
+    {
+        NavigableSet<Identifiable<Long>> receiver = new TreeSet<>();
+        receiver.add(receiverId);
+        return Users.of(receiver);
     }
 }
