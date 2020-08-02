4e689bf679a6642bb4c13c4be051598c47a247c2
Ryan Tatton <rdt17@protonmail.com>
2020-08-02 12:02:17 -0400

    Major-redesign
    - Removes Lombok annotations, besides @Log4j2, to avoid compiling issues.
    - Adds static constructors from DataInput and JsonNodes.
    - Implements AbstractRiskScore
    
    Signed-off-by: Ryan Tatton <rdt17@protonmail.com>

--- src/main/java/model/score/TemporalUserRiskScore.java
+++ src/main/java/model/score/TemporalUserRiskScore.java
@@ -1,80 +1,137 @@
 package model.score;
 
-import lombok.AccessLevel;
-import lombok.Data;
-import lombok.NoArgsConstructor;
-import lombok.NonNull;
-import lombok.Setter;
-import lombok.extern.log4j.Log4j2;
-import model.identity.UserId;
-import org.apache.hadoop.io.WritableComparable;
-
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonGetter;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.google.common.base.Preconditions;
 import java.io.DataInput;
 import java.io.DataOutput;
 import java.io.IOException;
+import java.text.MessageFormat;
 import java.time.Instant;
+import java.util.Objects;
+import lombok.extern.log4j.Log4j2;
+import model.identity.UserId;
+
+// TODO Change UserId to String
 
 /**
- * A {@link RiskScore} with a time for when it was last updated and an associated user.
+ * A {@link AbstractRiskScore} with a time for when it was last updated and an associated user.
  * <p>
- * The default implementation of {@link #compareTo(TemporalUserRiskScore)} is to first compare risk score update time.
- * If the risk scores are equally comparable based on the former, the value of the risk scores is then used for
- * comparison. Finally, if the risk scores are still equally comparable based on the former two criteria, the
- * {@link #userId} is used for comparison.
+ * The default implementation of {@link #compareTo(TemporalUserRiskScore)} is to first compare risk
+ * score update time. If the risk scores are equally comparable based on the former, the value of
+ * the risk scores is then used for comparison. Finally, if the risk scores are still equally
+ * comparable based on the former two criteria, the {@link #id} is used for comparison.
  *
  * @see TemporalRiskScore
  */
 @Log4j2
-@Data
-@Setter(AccessLevel.NONE)
-@NoArgsConstructor(access = AccessLevel.PRIVATE)
-public final class TemporalUserRiskScore implements WritableComparable<TemporalUserRiskScore>
-{
-    private UserId userId;
+public final class TemporalUserRiskScore implements AbstractRiskScore,
+    Comparable<TemporalUserRiskScore> {
 
-    private TemporalRiskScore temporalRiskScore;
+  private UserId id;
 
-    private TemporalUserRiskScore(UserId userId, TemporalRiskScore temporalRiskScore)
-    {
-        this.userId = userId;
-        this.temporalRiskScore = temporalRiskScore;
+  private TemporalRiskScore riskScore;
+
+  private TemporalUserRiskScore(UserId userId, TemporalRiskScore temporalRiskScore) {
+    Preconditions.checkNotNull(userId);
+    Preconditions.checkNotNull(temporalRiskScore);
+    id = UserId.of(userId.getId());
+    riskScore = TemporalRiskScore
+        .of(temporalRiskScore.getTimeUpdated(), temporalRiskScore.getRiskScore());
+  }
+
+  private TemporalUserRiskScore() {
+  }
+
+  @JsonCreator
+  public static TemporalUserRiskScore of(UserId userId, Instant updateTime, double riskScore) {
+    return new TemporalUserRiskScore(userId, TemporalRiskScore.of(updateTime, riskScore));
+  }
+
+  public static TemporalUserRiskScore fromJsonNode(JsonNode jsonNode) {
+    log.debug("Creating TemporalUserRiskScore from JsonNode");
+    Preconditions.checkNotNull(jsonNode);
+    UserId userId = UserId.fromJsonNode(jsonNode.get(UserId.getIdLabel()));
+    TemporalRiskScore riskScore = TemporalRiskScore.fromJsonNode(jsonNode);
+    return new TemporalUserRiskScore(userId, riskScore);
+  }
+
+  static TemporalUserRiskScore fromDataInput(DataInput dataInput) throws IOException {
+    log.debug("Creating TemporalUserRiskScore from DataInput");
+    Preconditions.checkNotNull(dataInput);
+    TemporalUserRiskScore riskScore = new TemporalUserRiskScore();
+    riskScore.readFields(dataInput);
+    return riskScore;
+  }
+
+  @Override
+  public void readFields(DataInput dataInput) throws IOException {
+    Preconditions.checkNotNull(dataInput);
+    id = UserId.fromDataInput(dataInput);
+    riskScore = TemporalRiskScore.fromDataInput(dataInput);
+  }
+
+  @Override
+  public void write(DataOutput dataOutput) throws IOException {
+    Preconditions.checkNotNull(dataOutput);
+    id.write(dataOutput);
+    riskScore.write(dataOutput);
+  }
+
+  @Override
+  public int compareTo(TemporalUserRiskScore o) {
+    Preconditions.checkNotNull(o);
+    int compare = getUpdateTime().compareTo(o.getUpdateTime());
+    if (0 == compare) {
+      compare = Double.compare(getRiskScore(), o.getRiskScore());
+      if (0 == compare) {
+        compare = id.compareTo(o.getUserId());
+      }
     }
+    return compare;
+  }
 
-    public static TemporalUserRiskScore of(@NonNull UserId userId, @NonNull Instant updateTime,
-                                           @NonNull RiskScore riskScore)
-    {
-        return new TemporalUserRiskScore(UserId.of(userId.getId()), TemporalRiskScore.of(updateTime, riskScore));
-    }
+  @JsonGetter
+  public UserId getUserId() {
+    return id;
+  }
 
-    public static TemporalUserRiskScore fromDataInput(DataInput dataInput) throws IOException
-    {
-        TemporalUserRiskScore riskScore = new TemporalUserRiskScore();
-        riskScore.readFields(dataInput);
-        return riskScore;
-    }
+  public Instant getUpdateTime() {
+    return riskScore.getTimeUpdated();
+  }
 
-    @Override
-    public void readFields(DataInput dataInput) throws IOException
-    {
-        userId = UserId.fromDataInput(dataInput);
-        temporalRiskScore = TemporalRiskScore.fromDataInput(dataInput);
-    }
+  @Override
+  public double getRiskScore() {
+    return riskScore.getRiskScore();
+  }
 
-    @Override
-    public void write(DataOutput dataOutput) throws IOException
-    {
-        userId.write(dataOutput);
-        temporalRiskScore.write(dataOutput);
+  @Override
+  public boolean equals(Object o) {
+    if (this == o) {
+      return true;
     }
+    if (o == null || getClass() != o.getClass()) {
+      return false;
+    }
+    TemporalUserRiskScore score = (TemporalUserRiskScore) o;
+    boolean equalId = Objects.equals(id, score.getUserId());
+    boolean equalUpdateTime = Objects.equals(getUpdateTime(), score.getUpdateTime());
+    boolean equalRiskScore = Objects.equals(getRiskScore(), score.getRiskScore());
+    return equalId && equalUpdateTime && equalRiskScore;
+  }
 
-    @Override
-    public int compareTo(@NonNull TemporalUserRiskScore o)
-    {
-        int compare = temporalRiskScore.compareTo(o.getTemporalRiskScore());
-        if (0 == compare)
-        {
-            compare = userId.compareTo(o.getUserId());
-        }
-        return compare;
-    }
+  @Override
+  public int hashCode() {
+    return Objects.hash(id, getRiskScore());
+  }
+
+  @Override
+  public String toString() {
+    return MessageFormat
+        .format("TemporalUserRiskScore'{'userId={0}, updateTime={1}, riskScore={2}'}'",
+            id,
+            riskScore.getTimeUpdated(),
+            riskScore.getRiskScore());
+  }
 }
