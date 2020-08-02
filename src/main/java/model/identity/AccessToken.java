e2bfa333f7b425e6222fe487c64c09bf1699066b
Ryan Tatton <rdt17@protonmail.com>
2020-08-02 10:18:57 -0400

    Major re-design
    - Removes Lombok annotations, besides @Log4j2 to avoid compiling issues.
    
    Signed-off-by: Ryan Tatton <rdt17@protonmail.com>

--- src/main/java/model/identity/AccessToken.java
+++ src/main/java/model/identity/AccessToken.java
@@ -1,31 +1,60 @@
 package model.identity;
 
-import lombok.AccessLevel;
-import lombok.Getter;
-import lombok.NonNull;
-import lombok.Value;
+import com.google.common.base.Preconditions;
+import com.google.common.base.Strings;
+import java.text.MessageFormat;
+import java.util.Objects;
 import lombok.extern.log4j.Log4j2;
 
 /**
  * A generic access token intended to be used for writing data to an authenticated entity.
+ *
+ * @see Identifiable
  */
 @Log4j2
-@Value(staticConstructor = "of")
-public class AccessToken implements Identifiable<String>
-{
-    @NonNull
-    @Getter(AccessLevel.NONE)
-    String token;
+public final class AccessToken implements Identifiable<String> {
 
-    @Override
-    public String getId()
-    {
-        return token;
-    }
+  private final String token;
 
-    @Override
-    public int compareTo(@NonNull Identifiable<String> o)
-    {
-        return token.compareTo(o.getId());
+  private AccessToken(String s) {
+    Preconditions.checkArgument(Strings.isNullOrEmpty(s));
+    token = s;
+  }
+
+  public static AccessToken of(String token) {
+    return new AccessToken(token);
+  }
+
+  @Override
+  public String getId() {
+    return token;
+  }
+
+  @Override
+  public int compareTo(Identifiable<String> o) {
+    Preconditions.checkNotNull(o);
+    return token.compareTo(o.getId());
+  }
+
+  @Override
+  public boolean equals(Object o) {
+    if (this == o) {
+      return true;
     }
+    if (null == o || getClass() != o.getClass()) {
+      return false;
+    }
+    Identifiable<String> accessToken = (Identifiable<String>) o;
+    return Objects.equals(token, accessToken.getId());
+  }
+
+  @Override
+  public int hashCode() {
+    return Objects.hash(token);
+  }
+
+  @Override
+  public String toString() {
+    return MessageFormat.format("AccessToken'{'token=''{0}'''}'", token);
+  }
 }
