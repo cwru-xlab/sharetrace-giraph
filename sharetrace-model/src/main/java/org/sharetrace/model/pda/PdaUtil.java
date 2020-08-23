7b6e6b691e92157969618f8ff9b7aaba76740aab
Ryan Tatton <rdt17@protonmail.com>
2020-08-23 17:56:46 -0400

    Adds PdaUtil
    The purpose of this class is to refactor repetitive verification of Optional types, such as String and any type that subclasses Number.
    
    Signed-off-by: Ryan Tatton <rdt17@protonmail.com>

---
+++ sharetrace-model/src/main/java/org/sharetrace/model/pda/PdaUtil.java
@@ -0,0 +1,25 @@
+package org.sharetrace.model.pda;
+
+import com.google.common.base.Preconditions;
+import com.google.common.base.Strings;
+import java.util.Optional;
+import java.util.function.Predicate;
+
+public final class PdaUtil {
+
+  public static void verifyOptionalString(Optional<String> stringOptional,
+      String exceptionMessage) {
+    if (stringOptional.isPresent()) {
+      String string = stringOptional.get();
+      Preconditions.checkArgument(!Strings.isNullOrEmpty(string), exceptionMessage);
+    }
+  }
+
+  public static <N extends Number> void verifyOptionalNumber(Optional<N> numberOptional,
+      Predicate<N> condition, String exceptionMessage) {
+    if (numberOptional.isPresent()) {
+      N number = numberOptional.get();
+      Preconditions.checkArgument(condition.test(number), exceptionMessage);
+    }
+  }
+}
