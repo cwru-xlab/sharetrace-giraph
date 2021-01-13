3a148a32eba9df2f212261b7fe393a1ccd6ca4f2
Ryan Tatton <rdt17@protonmail.com>
2021-01-13 11:36:13 -0500

    Adds ray context and uses numpy array for factor and variable storage
    Signed-off-by: Ryan Tatton <rdt17@protonmail.com>

--- sharetrace-python/backend.py
+++ sharetrace-python/backend.py
@@ -1,13 +1,23 @@
 import abc
-from typing import Any, Collection, Hashable, Iterable, Mapping, NoReturn, Tuple
+import contextlib
+from typing import Any, Callable, Hashable, Iterable, Mapping, NoReturn, Tuple
 
 import attr
 import igraph
 import networkx
+import numpy as np
+import psutil
+import ray
 
 NETWORKX = 'networkx'
 IGRAPH = 'igraph'
 OPTIONS = {NETWORKX, IGRAPH}
+NUM_CPUS = psutil.cpu_count(logical=False)
+LOGGER: Callable = print
+
+
+def _array_factory():
+	return np.empty((0,))
 
 
 @attr.s(slots=True)
@@ -65,8 +75,8 @@ class FactorGraph(abc.ABC):
 @attr.s(slots=True)
 class IGraphFactorGraph(FactorGraph):
 	_graph = attr.ib(type=igraph.Graph, factory=igraph.Graph)
-	_factors = attr.ib(type=Iterable[Hashable], factory=frozenset)
-	_variables = attr.ib(type=Iterable[Hashable], factory=frozenset)
+	_factors = attr.ib(type=Iterable[Hashable], factory=_array_factory)
+	_variables = attr.ib(type=Iterable[Hashable], factory=_array_factory)
 
 	def __attrs_post_init__(self):
 		super(IGraphFactorGraph, self).__init__()
@@ -76,16 +86,16 @@ class IGraphFactorGraph(FactorGraph):
 		return self._factors
 
 	@factors.setter
-	def factors(self, value: Collection[Hashable]) -> NoReturn:
-		self._factors = value
+	def factors(self, value: Iterable[Hashable]) -> NoReturn:
+		self._factors = np.array(list(value))
 
 	@property
 	def variables(self) -> Iterable[Hashable]:
 		return self._variables
 
 	@variables.setter
-	def variables(self, value: Collection[Hashable]) -> NoReturn:
-		self._variables = value
+	def variables(self, value: Iterable[Hashable]) -> NoReturn:
+		self._variables = np.array(list(value))
 
 	def get_neighbors(self, vertex: Hashable) -> Iterable[igraph.Vertex]:
 		return self._graph.neighbors(vertex)
@@ -122,8 +132,8 @@ class IGraphFactorGraph(FactorGraph):
 @attr.s(slots=True)
 class NetworkXFactorGraph(FactorGraph):
 	_graph = attr.ib(type=networkx.Graph, factory=networkx.Graph)
-	_factors = attr.ib(type=Iterable[Hashable], factory=frozenset)
-	_variables = attr.ib(type=Iterable[Hashable], factory=frozenset)
+	_factors = attr.ib(type=Iterable[Hashable], factory=_array_factory)
+	_variables = attr.ib(type=Iterable[Hashable], factory=_array_factory)
 
 	def __attrs_post_init__(self):
 		super(NetworkXFactorGraph, self).__init__()
@@ -133,16 +143,16 @@ class NetworkXFactorGraph(FactorGraph):
 		return self._factors
 
 	@factors.setter
-	def factors(self, value: Hashable) -> NoReturn:
-		self._factors = value
+	def factors(self, value: Iterable[Hashable]) -> NoReturn:
+		self._factors = np.array(list(value))
 
 	@property
 	def variables(self) -> Iterable[Hashable]:
 		return self._variables
 
 	@variables.setter
-	def variables(self, value: Hashable) -> NoReturn:
-		self._variables = value
+	def variables(self, value: Iterable[Hashable]) -> NoReturn:
+		self._variables = np.array(list(value))
 
 	def get_neighbors(self, vertex: Hashable) -> Iterable[Hashable]:
 		return self._graph.neighbors(vertex)
@@ -175,3 +185,11 @@ class NetworkXFactorGraph(FactorGraph):
 			edges: Iterable[Tuple[Hashable, Hashable]],
 			attributes: Mapping) -> NoReturn:
 		self._graph.add_edges_from(((*e, attributes[e]) for e in edges))
+
+
+@contextlib.contextmanager
+def ray_context(*args, **kwargs):
+	try:
+		yield ray.init(*args, **kwargs)
+	finally:
+		ray.shutdown()
