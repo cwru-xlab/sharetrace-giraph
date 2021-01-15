a15e3375fc48d9a44629511f1d98c3f299fcd577
Ryan Tatton <rdt17@protonmail.com>
2021-01-15 10:25:15 -0500

    Checkpoint before making the final stretch
    Will be addressing the several TODOs in this module. Want a point of reference before moving further.
    
    Signed-off-by: Ryan Tatton <rdt17@protonmail.com>

--- sharetrace-python/algorithm.py
+++ sharetrace-python/algorithm.py
@@ -1,17 +1,31 @@
 import collections
 import datetime
+import functools
 import itertools
 import random
 from typing import (
-	Any, DefaultDict, Generator, Hashable, Iterable, Mapping, NoReturn, Set,
-	Tuple)
+	Any, DefaultDict, Hashable, Iterable, Mapping, NoReturn, Set,
+	Sized, Tuple)
 
 import attr
 import codetiming
 import numpy as np
+import ray
+from ray.util import iter as it, queue
 
 import backend
+import graphs
 import model
+from backend import NUM_CPUS
+from graphs import FactorGraph, Vertex
+
+"""
+Optimization best practices:
+	- Use itertools over custom implementations
+	- Use generators for intermediate processing results
+	- Use numpy array over standard list or tuple
+	- Only make copies of mutable collections if modifying them
+"""
 
 _TWO_DAYS = datetime.timedelta(days=2)
 _DEFAULT_MESSAGE = model.RiskScore(
@@ -20,12 +34,6 @@ _RiskScores = Iterable[model.RiskScore]
 _GroupedRiskScores = Iterable[Tuple[str, Iterable[model.RiskScore]]]
 _Contacts = Iterable[model.Contact]
 log = backend.LOGGER
-"""
-Optimization best practices:
-	- Use itertools over custom implementations
-	- Use generators for intermediate processing results
-	- Use numpy array over standard list or tuple
-"""
 
 
 @attr.s(slots=True)
@@ -71,18 +79,16 @@ class BeliefPropagation:
 	transmission_rate = attr.ib(type=float, default=0.8, converter=float)
 	tolerance = attr.ib(type=float, default=1e-5, converter=float)
 	iterations = attr.ib(type=int, default=4, converter=int)
-	_backend = attr.ib(type=str, default=backend.IGRAPH)
-	_graph = attr.ib(type=backend.FactorGraph, init=False)
+	max_messages = attr.ib(type=int, default=None)
+	backend = attr.ib(type=str, default=graphs.IGRAPH)
+	_graph = attr.ib(type=FactorGraph, init=False)
+	_queue = attr.ib(type=queue.Queue, init=False)
 	_seed = attr.ib(default=None)
 
 	def __attrs_post_init__(self):
-		if self._backend == backend.IGRAPH:
-			self._graph = backend.IGraphFactorGraph()
-		elif self._backend == backend.NETWORKX:
-			self._graph = backend.NetworkXFactorGraph()
-		else:
-			raise AttributeError(
-				f'Backend must be one of the following: {backend.OPTIONS}')
+		self._graph = graphs.RayFactorGraph(backend=self.backend).remote()
+		max_messages = 0 if self.max_messages is None else self.max_messages
+		self._queue = queue.Queue(maxsize=max_messages)
 		if self._seed is not None:
 			random.seed(self._seed)
 			np.random.seed(self._seed)
@@ -103,123 +109,146 @@ class BeliefPropagation:
 		if value < 1:
 			raise ValueError('Iterations must be at least 1')
 
+	@max_messages.validator
+	def _check_max_messages(self, attribute, value):
+		if attribute is not None:
+			if not isinstance(attribute, int):
+				raise TypeError('Max messages must of type int or None')
+			if attribute < 0:
+				raise ValueError('Max messages must be least 1')
+
 	def __call__(
 			self,
 			factors: _Contacts,
 			variables: _GroupedRiskScores
-	) -> Iterable[Tuple[Hashable, model.RiskScore]]:
+	) -> Iterable[Tuple[Vertex, model.RiskScore]]:
 		txt = 'BELIEF PROPAGATION: {:0.6f} s'
 		with codetiming.Timer(text=txt, logger=log):
 			return self.call(factors=factors, variables=variables)
+
+	# TODO IMPORTANT: If using a queue to store messages, then the node data
+	#  is the only stateful component that can be stored separate from the
+	#  graph itself. In this way, the graph can be built and pinned in memory
+	#  to be shared by all tasks needing to access it. Thus, the Actor
+	#  required is something like a VertexStore that maintains the state of
+	#  each of the vertices (i.e. previous, local, and max)
+
+	# TODO Design signal to switch between send_to_variables and
+	#  send_to_factors
+	# TODO Design Actor to store and update state of nodes
+	# TODO Modify design to not store edge data
+	# TODO Implement Message class
 
 	def call(
 			self,
 			factors: _Contacts,
 			variables: _GroupedRiskScores
-	) -> Iterable[Tuple[Hashable, model.RiskScore]]:
+	) -> Iterable[Tuple[Vertex, model.RiskScore]]:
 		with codetiming.Timer(text='Creating the graph: {:0.6f} s'):
 			self._create_graph(factors, variables)
-		max_risks = np.array(self._get_max_risks())
-		iteration, tolerance = 0, np.inf
+		maxes = self._get_all_max()
+		i, t = 0, np.inf
 		log('-----------Start of algorithm-----------')
-		while iteration < self.iterations and tolerance > self.tolerance:
-			log(f'-----------Iteration {iteration + 1}-----------')
+		while i < self.iterations and t > self.tolerance:
+			log(f'-----------Iteration {i + 1}-----------')
 			with codetiming.Timer(text='To factors: {:0.6f} s', logger=log):
-				self._send_to_factors()
+				signal = None
+				self._send_to_factors(signal)
 			with codetiming.Timer(text='To variables: {:0.6f} s', logger=log):
-				self._send_to_variables()
-			iteration += 1
+				self._send_to_variables(signal)
+			i += 1
 			with codetiming.Timer(text='Updating: {:0.6f} s', logger=log):
-				iter_risks = self._update_max_risks()
-				tolerance = sum(iter_risks - max_risks)
-				max_risks = iter_risks
-				if iteration < self.iterations:
-					self._copy_to_previous()
+				iter_maxes = self._update_max()
+				t = sum(iter_maxes - maxes)
+				maxes = iter_maxes
+				if i < self.iterations:
+					self._set_previous()
 					self._clear_messages()
-			log(f'Tolerance: {round(tolerance, 6)}')
+			log(f'Tolerance: {round(t, 6)}')
 		log('-----------End of algorithm-----------')
-		return ((v, self._get_max_risk(v)) for v in self._graph.variables)
-
-	def _update_max_risks(self) -> np.ndarray:
-		updated = []
-		for v in self._graph.variables:
-			messages = self._get_messages_to_variable(v).values()
-			max_risk = self._get_max_risk(v)
-			max_risk = _chain_max(messages, [max_risk], key=lambda r: r.value)
-			self._set_max_risk(v, max_risk)
-			updated.append(max_risk.value)
-		return np.array(updated)
+		return ((v, self._get_max(v)) for v in self._graph.get_variables())
 
 	def _create_graph(
 			self,
 			factors: _Contacts,
 			variables: _GroupedRiskScores) -> NoReturn:
-		# Must create variable vertices first before creating edges
+		# Must create variables first before creating edges
 		self._add_variables(variables)
-		self._add_factors(factors)
+		self._add_factors_and_edges(factors)
 
-	def _add_factors(self, factors: _Contacts) -> NoReturn:
+	def _add_variables(self, variables: _GroupedRiskScores):
+		keys = []
+		attrs = {}
+		for k, v in variables:
+			v1, v2 = itertools.tee(v)
+			keys.append(k)
+			attrs.update({
+				k: {
+					'local': _local_factory(v1),
+					'max': max(v2),
+					'previous': _previous_factory()}})
+		self._graph.add_vertices(keys, attrs)
+		self._graph.set_variables(keys)
+
+	def _add_factors_and_edges(self, factors: _Contacts):
 		def make_key(factor: model.Contact):
 			parts = [str(u) for u in sorted(factor.users)]
 			key = '_'.join(parts)
 			return key, parts
 
-		factors = np.array([(make_key(f), f) for f in factors], dtype=object)
-		keys = frozenset(k for ((k, _), _) in factors)
-		self._graph.factors = keys
-		attrs = {k: {'occurrences': f.occurrences} for ((k, _), f) in factors}
-		self._graph.add_vertices(keys, attrs)
-		edges = itertools.chain(
-			((p, k) for ((k, (p, _)), _) in factors),
-			((p, k) for ((k, (_, p)), _) in factors))
-		edges1, edges2 = itertools.tee(edges)
-		attrs = {e: _edge_data_factory() for e in edges1}
-		self._graph.add_edges(edges2, attrs)
+		vertices, edges = [], []
+		vertex_attrs, edge_attrs = {}, {}
+		for f in factors:
+			k, (v1, v2) = make_key(f)
+			vertices.append(k)
+			vertex_attrs.update({k: {'occurrences': f.occurrences}})
+			e1, e2 = (k, v1), (k, v2)
+			edges.extend((e1, e2))
+			# TODO Don't need this anymore
+			edge_attrs.update({
+				e1: _edge_data_factory(), e2: _edge_data_factory()})
+		self._graph.set_factors(vertices)
+		self._graph.add_vertices(vertices, vertex_attrs)
+		self._graph.add_edges(edges, edge_attrs)
 
-	def _add_variables(self, variables: _GroupedRiskScores) -> NoReturn:
-		variables = np.array(
-			[(str(k), np.array(list(v))) for k, v in variables], dtype=object)
-		keys = frozenset(k for k, _ in variables)
-		self._graph.variables = keys
-		attrs = {
-			k: {
-				'local_risks': _local_factory(v),
-				'max_risk': max(v),
-				'previous': _previous_factory()}
-			for k, v in variables}
-		self._graph.add_vertices(keys, attrs)
+	def _send_to_variables(self, signal):  # TODO Signal
+		factors = self._graph.get_factors()
+		for shard in _get_index_batches(factors).shards():
+			self._to_variables.remote(
+				self._graph,
+				self._queue,
+				shard,
+				self.transmission_rate,
+				signal)
 
-	def _send_to_variables(self) -> NoReturn:
-		for factor, variable in self._graph_iter(self._graph.factors):
-			neighbor = self._get_neighbor(factor, variable)
-			local = self._get_local_values(neighbor)
-			previous = self._get_previous(neighbor).values()
-			messages = itertools.chain(local, previous)
-			message = self._compute_message(factor, messages)
-			self._send_to_variable(factor, variable, message)
-
-	def _graph_iter(
+	@ray.remote
+	def _to_variables(
 			self,
-			outer: Iterable[Hashable]
-	) -> Generator[Tuple[Hashable, Hashable], None, None]:
-		"""Generates node-neighbor tuples.
-
-		A cartesian product of the outer iterable with neighbors of each of
-		the outer iterable elements is iterated over.
-
-		Args:
-			outer: Elements over which to iterate and generate neighbors.
-
-		Returns:
-			A generator with each element being a tuple of the form (outer
-			element, neighbor of outer element).
-		"""
-		for o in outer:
-			for n in self._graph.get_neighbors(o):
-				yield o, n
+			graph: FactorGraph,
+			msg_queue: queue.Queue,
+			indices: Iterable[np.ndarray],
+			transmission_rate: float,
+			signal):
+		factors = graph.get_factors()
+		for f in itertools.chain(*(factors[i] for i in indices)):
+			neighbors = np.array(list(graph.get_neighbors(f)))
+			for i, v in enumerate(neighbors):
+				neighbor = neighbors[not i]
+				local = self._graph.get_vertex_attr(
+					vertex=neighbor, key='local')
+				previous = self._get_previous(graph, neighbor)
+				messages = itertools.chain(local, previous)
+				message = self._compute_message(
+					graph, f, messages, transmission_rate)
+				# block = self.max_messages is None
+				msg_queue.put({(f, v): message})
 
 	def _compute_message(
-			self, factor: Hashable, messages: _RiskScores) -> NoReturn:
+			self,
+			graph: FactorGraph,
+			factor: Vertex,
+			messages: _RiskScores,
+			transmission_rate: float) -> model.RiskScore:
 		"""Computes messages to send from a factor node to a variable node.
 
 		Only messages that occurred sufficiently before at least one factor
@@ -238,10 +267,10 @@ class BeliefPropagation:
 		"""
 		messages = np.array(
 			sorted(messages, key=lambda m: m.timestamp, reverse=True))
-		value = self._get_factor_value(factor)
+		occurs = self._graph.get_vertex_attr(vertex=factor, key='occurrences')
 		recent = np.array([
 			(i, m) for i, m in enumerate(messages)
-			if any(m.timestamp <= v.timestamp + _TWO_DAYS for v in value)])
+			if any(m.timestamp <= o.timestamp + _TWO_DAYS for o in occurs)])
 		if len(recent) == 0:
 			message = _DEFAULT_MESSAGE
 		else:
@@ -252,113 +281,103 @@ class BeliefPropagation:
 				(i, sum(m.value * np.exp(-d) for d in range(i)))
 				for i, m in recent)
 			weighted = (
-				(i, w * self.transmission_rate / norm)
+				(i, w * transmission_rate / norm)
 				for (i, w), norm in zip(weighted, norms))
 			i, _ = max(weighted, key=lambda x: x[1])
 			message = messages[i]
 		return message
 
-	def _send_to_factors(self) -> NoReturn:
-		for variable, factor in self._graph_iter(self._graph.variables):
-			local = self._get_local_values(variable)
-			from_others = self._get_from_other_factors(factor, variable)
-			messages = itertools.chain(local, from_others.values())
-			self._send_to_factor(factor, variable, messages)
+	def _send_to_factors(self, signal):  # TODO Signal
+		variables = self._graph.get_variables()
+		indices = _get_index_batches(variables)
+		for shard in indices.shards():
+			self._to_factors.remote(
+				self._graph, variables, self._queue, shard, signal)
 
-	# Edge getters / setters
-	def _get_messages_to_variable(
-			self, variable: Hashable) -> Mapping[Hashable, model.RiskScore]:
+	@ray.remote
+	def _to_factors(
+			self,
+			graph: FactorGraph,
+			variables: np.ndarray,
+			msg_queue: queue.Queue,
+			indices: Iterable[np.ndarray],
+			signal):
+		for v in itertools.chain(*(variables[i] for i in indices)):
+			local = graph.get_vertex_attr(vertex=v, key='local')
+			for f in graph.get_neighbors(v):
+				from_others = self._get_from_other_factors(graph, f, v)
+				messages = itertools.chain(local, from_others.values())
+				# block = self.max_messages is None
+				msg_queue.put({(f, v): messages})
+
+	def _get_to_variable(
+			self, variable: Vertex) -> Mapping[Vertex, model.RiskScore]:
 		to_variable = _previous_factory()
 		to_variable.update({
 			f: self._graph.get_edge_attr(edge=(variable, f), key='to_variable')
 			for f in self._graph.get_neighbors(variable)})
 		return to_variable
 
-	def _get_from_other_factors(
-			self,
-			factor: Hashable,
-			variable: Hashable) -> Mapping[Hashable, model.RiskScore]:
-		"""Get all messages received by a variable node from all other of
-		its neighboring factor nodes."""
-		to_variable = _previous_factory()
-		to_variable.update({
-			f: msg for f, msg in self._get_previous(variable).items()
-			if f != factor})
-		return to_variable
+	def _set_previous(self) -> NoReturn:
+		for v in self._graph.get_variables():
+			msg = self._get_to_variable(v)
+			self._graph.set_vertex_attr(vertex=v, key='previous', value=msg)
 
-	def _send_to_factor(
-			self,
-			factor: Hashable,
-			variable: Hashable,
-			messages: Iterable[model.RiskScore]) -> NoReturn:
-		edge = (variable, factor)
-		value = self._graph.get_edge_attr(edge=edge, key='to_factor')
-		value.update(messages)
-		self._graph.set_edge_attr(edge=edge, key='to_factor', value=value)
+	def _get_all_max(self) -> np.ndarray:
+		variables = self._graph.get_variables()
+		return np.array([self._get_max(v).value for v in variables])
 
-	def _send_to_variable(
-			self,
-			factor: Hashable,
-			variable: Hashable,
-			message: model.RiskScore) -> NoReturn:
-		self._graph.set_edge_attr(
-			edge=(variable, factor), key='to_variable', value=message)
+	def _get_max(self, variable: Vertex) -> model.RiskScore:
+		return self._graph.get_vertex_attr(vertex=variable, key='max')
 
-	def _copy_to_previous(self) -> NoReturn:
-		for v in self._graph.variables:
-			messages = self._get_messages_to_variable(v)
-			self._set_previous(v, messages)
+	def _update_max(self) -> np.ndarray:
+		updated = []
+		for v in self._graph.get_variables():
+			msg = self._get_to_variable(v).values()
+			mx = self._get_max(v)
+			mx = _chain_max(msg, [mx], key=lambda r: r.value)
+			self._graph.set_vertex_attr(vertex=v, key='max', value=mx)
+			updated.append(mx.value)
+		return np.array(updated)
 
 	def _clear_messages(self):
-		for v, f in self._graph_iter(self._graph.variables):
+		variables = self._graph.get_variables()
+		neighbors = (self._graph.get_neighbors(v) for v in variables)
+		for v, f in itertools.product(variables, neighbors):
 			self._graph.set_edge_attr(
 				edge=(v, f), key='to_variable', value=None)
 			self._graph.set_edge_attr(
 				edge=(v, f), key='to_factor', value=_to_factor_factory())
 
-	# Factor node getters / setters
-	def _get_factor_value(
-			self, factor: Hashable) -> Iterable[model.Occurrence]:
-		return self._graph.get_vertex_attr(vertex=factor, key='occurrences')
+	@staticmethod
+	def _get_from_other_factors(
+			graph: FactorGraph,
+			factor: Vertex,
+			variable: Vertex) -> Mapping[Vertex, model.RiskScore]:
+		to_variable = _previous_factory()
+		get_previous = functools.partial(BeliefPropagation._get_previous)
+		to_variable.update({
+			f: msg for f, msg in get_previous(graph, variable).items()
+			if f != factor})
+		return to_variable
 
-	# Variable node getters / setters
-	def _get_neighbor(self, factor: Hashable, variable: Hashable) -> Hashable:
-		neighbors = iter(self._graph.get_neighbors(factor))
-		neighbor = next(neighbors)
-		return neighbor if neighbor != variable else next(neighbors)
-
-	def _get_local_values(
-			self, variable: Hashable) -> Iterable[model.RiskScore]:
-		local = self._graph.get_vertex_attr(vertex=variable, key='local_risks')
-		return _local_factory(local)
-
+	@staticmethod
 	def _get_previous(
-			self, variable: Hashable) -> Mapping[Hashable, model.RiskScore]:
-		previous = self._graph.get_vertex_attr(vertex=variable, key='previous')
-		return _previous_factory(previous)
-
-	def _set_previous(
-			self,
-			variable: Hashable,
-			value: Mapping[Hashable, model.RiskScore]) -> NoReturn:
-		self._graph.set_vertex_attr(
-			vertex=variable, key='previous', value=value)
-
-	def _get_max_risks(self) -> Iterable[model.RiskScore]:
-		max_risks = (
-			self._get_max_risk(v).value for v in self._graph.variables)
-		return np.array(list(max_risks))
-
-	def _get_max_risk(self, variable: Hashable) -> model.RiskScore:
-		return self._graph.get_vertex_attr(vertex=variable, key='max_risk')
-
-	def _set_max_risk(
-			self, variable: Hashable, value: model.RiskScore) -> NoReturn:
-		self._graph.set_vertex_attr(
-			vertex=variable, key='max_risk', value=value)
+			graph: FactorGraph,
+			variable: Vertex) -> Mapping[Vertex, model.RiskScore]:
+		return graph.get_vertex_attr(vertex=variable, key='previous')
 
 
-def _previous_factory(values=None) -> DefaultDict[Hashable, model.RiskScore]:
+def _get_index_batches(vertices: Sized) -> it.ParallelIterator:
+	num_vertices = len(vertices)
+	indices = it.from_range(num_vertices, num_shards=NUM_CPUS)
+	# Guarantees multiple batches for multiple CPUs and a max size of 100
+	batch_size = int(np.ceil(num_vertices / NUM_CPUS ** 2))
+	batch_size = min(100, batch_size)
+	return indices.batch(batch_size).for_each(np.array)
+
+
+def _previous_factory(values=None) -> DefaultDict[Vertex, model.RiskScore]:
 	factory = collections.defaultdict(None)
 	if values is not None:
 		factory.update(values)
