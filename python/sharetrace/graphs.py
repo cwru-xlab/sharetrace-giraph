38e06f98ead8734590873b0cdfcd4b042aac7eae
Ryan Tatton <rdt17@protonmail.com>
2021-01-23 10:54:12 -0500

    Docstrings, module renames, type hints, and updated FactorGraphBuilder
    - Docstrings added to all FactorGraph classes.
    - Adds missing type hints.
    - FactorGraphBuilder updated to allow for multiple VertexStores.
    - `backend` parameter renamed to `impl` to avoid name collision.
    
    Signed-off-by: Ryan Tatton <rdt17@protonmail.com>

--- python/sharetrace/graphs.py
+++ python/sharetrace/graphs.py
@@ -1,7 +1,9 @@
 import abc
 import collections
-from typing import Any, Collection, Hashable, Iterable, Mapping, NoReturn, \
-	Optional, Tuple, Union
+import itertools
+from typing import (
+	Any, Hashable, Iterable, Mapping, NoReturn, Optional, Sequence, Tuple,
+	Union)
 
 import attr
 import igraph
@@ -9,11 +11,15 @@ import networkx
 import numpy as np
 import ray
 
-from _stores import VertexStore
+import backend
+from stores import VertexStore
 
 Edge = Tuple[Hashable, Hashable]
 Attributes = Mapping[str, Any]
 Vertex = Hashable
+VertexVector = Sequence[Tuple[Vertex, int]]
+VertexMatrix = Sequence[Sequence[Tuple[Vertex, int]]]
+OptionalVertexStores = Optional[Sequence[VertexStore]]
 NETWORKX = 'networkx'
 IGRAPH = 'igraph'
 NUMPY = 'numpy'
@@ -22,10 +28,17 @@ DEFAULT = NUMPY
 _EDGE_ATTRIBUTE_EXCEPTION = '{} does not support edge attributes'
 _VERTEX_ATTRIBUTE_EXCEPTION = '{} does not support vertex attributes'
 _KILL_EXCEPTION = '{} does not support kill() as it is not a Ray Actor'
-_BACKEND_EXCEPTION = f'Backend must be one of the following: {OPTIONS}'
+_IMPL_EXCEPTION = f"'impl' must be one of the following: {OPTIONS}"
+_NUM_STORES_CPU_EXCEPTION = (
+	"'num_stores' must be less than the number of physical cpus: {}")
+_MIN_NUM_STORES_EXCEPTION = "'num_stores' must be at least 1"
+_GRAPH_STATE_EXCEPTION = (
+	"Only one of 'graph_as_actor' and 'share_graph' can be set to True")
 
 
+# AttributeError results when using attrs
 class FactorGraph(abc.ABC):
+	"""An abstract implementation of a factor graph."""
 	__slots__ = []
 
 	def __init__(self):
@@ -85,9 +98,8 @@ class FactorGraph(abc.ABC):
 
 
 class IGraphFactorGraph(FactorGraph):
+	"""A factor graph implemented using the python-igraph package."""
 	__slots__ = ['_graph', '_factors', '_variables']
-
-	NAME = 'name'
 
 	def __init__(self):
 		super(IGraphFactorGraph, self).__init__()
@@ -110,8 +122,8 @@ class IGraphFactorGraph(FactorGraph):
 	def get_neighbors(
 			self, vertex: Vertex) -> Union[Iterable[str], Iterable[int]]:
 		neighbors = self._graph.neighbors(vertex)
-		if self.NAME in self._graph.vs.attribute_names():
-			neighbors = (self._graph.vs[n][self.NAME] for n in neighbors)
+		if 'name' in self._graph.vs.attribute_names():
+			neighbors = (self._graph.vs[n]['name'] for n in neighbors)
 		return neighbors
 
 	def get_vertex_attr(self, vertex: Vertex, key: Hashable) -> Any:
@@ -168,6 +180,7 @@ class IGraphFactorGraph(FactorGraph):
 
 
 class NetworkXFactorGraph(FactorGraph):
+	"""A factor graph implemented using the networkx package."""
 	__slots__ = ['_graph', '_factors', '_variables']
 
 	def __init__(self):
@@ -240,6 +253,15 @@ class NetworkXFactorGraph(FactorGraph):
 
 
 class NumpyFactorGraph(FactorGraph):
+	"""A factor graph implemented using a dictionary and numpy arrays.
+
+	Notes:
+		The key is a vertex identifier and the value is a numpy array of the
+		neighbors of the vertex.
+
+		Vertex and edge attributes are not supported. Use the VertexStore to
+		store vertex attributes.
+	"""
 	__slots__ = ['_graph', '_variables', '_factors']
 
 	def __init__(self):
@@ -260,32 +282,43 @@ class NumpyFactorGraph(FactorGraph):
 	def set_variables(self, value: Iterable[Vertex]) -> NoReturn:
 		self._variables = set(value)
 
-	def get_neighbors(self, vertex):
+	def get_neighbors(self, vertex: Vertex) -> Iterable[Vertex]:
 		return self._graph[vertex]
 
-	def get_vertex_attr(self, vertex, key):
+	def get_vertex_attr(self, vertex: Vertex, key: Hashable) -> Any:
 		cls = self.__class__.__name__
 		raise NotImplementedError(_VERTEX_ATTRIBUTE_EXCEPTION.format(cls))
 
-	def set_vertex_attr(self, vertex, key, value):
+	def set_vertex_attr(
+			self, vertex: Vertex, key: Hashable, value: Any) -> NoReturn:
 		cls = self.__class__.__name__
 		raise NotImplementedError(_VERTEX_ATTRIBUTE_EXCEPTION.format(cls))
 
-	def get_edge_attr(self, edge, key):
+	def get_edge_attr(self, edge: Edge, key: Hashable) -> Any:
 		cls = self.__class__.__name__
 		raise NotImplementedError(_EDGE_ATTRIBUTE_EXCEPTION.format(cls))
 
-	def set_edge_attr(self, edge, key, value):
+	def set_edge_attr(self, edge: Edge, key: Hashable, value: Any) -> NoReturn:
 		cls = self.__class__.__name__
 		raise NotImplementedError(_EDGE_ATTRIBUTE_EXCEPTION.format(cls))
 
-	def add_variables(self, vertices, attributes=None):
+	def add_variables(
+			self,
+			vertices: Iterable[Vertex],
+			attributes: Mapping[Vertex, Attributes] = None) -> NoReturn:
 		self._add_vertices(vertices, attributes, variables=True)
 
-	def add_factors(self, vertices, attributes=None):
+	def add_factors(
+			self,
+			vertices: Iterable[Vertex],
+			attributes: Mapping[Vertex, Attributes] = None) -> NoReturn:
 		self._add_vertices(vertices, attributes, variables=False)
 
-	def _add_vertices(self, vertices, attributes=None, variables=True):
+	def _add_vertices(
+			self,
+			vertices: Iterable[Vertex],
+			attributes: Mapping[Vertex, Attributes] = None,
+			variables: bool = True) -> NoReturn:
 		if attributes is not None:
 			cls = self.__class__.__name__
 			raise NotImplementedError(_VERTEX_ATTRIBUTE_EXCEPTION.format(cls))
@@ -295,7 +328,10 @@ class NumpyFactorGraph(FactorGraph):
 			self._factors.update(vertices)
 		self._graph.fromkeys(vertices, np.array([]))
 
-	def add_edges(self, edges, attributes=None):
+	def add_edges(
+			self,
+			edges: Iterable[Edge],
+			attributes: Mapping[Edge, Attributes] = None) -> NoReturn:
 		if attributes is not None:
 			cls = self.__class__.__name__
 			raise NotImplementedError(_EDGE_ATTRIBUTE_EXCEPTION.format(cls))
@@ -303,20 +339,20 @@ class NumpyFactorGraph(FactorGraph):
 			self._graph[v1] = np.append(self._graph[v1], np.array([v2]))
 			self._graph[v2] = np.append(self._graph[v2], np.array([v1]))
 
-	def kill(self):
+	def kill(self) -> NoReturn:
 		cls = self.__class__.__name__
 		raise NotImplementedError(_KILL_EXCEPTION.format(cls))
 
 
 # noinspection PyUnresolvedReferences
 class RayFactorGraph(FactorGraph):
-	__slots__ = ['backend', '_graph']
+	__slots__ = ['impl', '_graph']
 
-	def __init__(self, *, backend: str = DEFAULT, detached: bool = True):
+	def __init__(self, *, impl: str = DEFAULT, detached: bool = True):
 		super(RayFactorGraph, self).__init__()
 		self._graph = _factor_graph_factory(
-			backend=backend, as_actor=True, detached=detached)
-		self.backend = backend
+			impl=impl, as_actor=True, detached=detached)
+		self.impl = impl
 		self.detached = detached
 
 	def get_factors(self) -> Iterable[Vertex]:
@@ -370,54 +406,105 @@ class RayFactorGraph(FactorGraph):
 
 
 @attr.s(slots=True)
-class _FactorGraphBuilder:
-	backend = attr.ib(type=str, default=DEFAULT)
-	as_actor = attr.ib(type=bool, default=True, converter=bool)
+class FactorGraphBuilder:
+	"""Builder for instantiating FactorGraph implementations.
+
+	Attributes:
+		impl: Package implementation of the factor graph.
+		share_graph: True pins the FactorGraph in shared object store memory
+			using the ray package.
+		graph_as_actor: True instantiates the graph as an actor using the ray
+			package.
+		use_vertex_store: True stores vertex attributes in a VertexStore.
+		num_stores: If greater than 1, vertices are round-robin assigned to
+			multiple vertex stores.
+		store_as_actor: True instantiates the VertexStore as an actor using
+			the ray package.
+		detatched: True instantiates the FactorGraph and VertexStore as
+			detached actors, using the ray package. Has no effect of the
+			corresponding flag parameter for using as an actor is set to False.
+	"""
+	impl = attr.ib(type=str, default=DEFAULT)
 	share_graph = attr.ib(type=bool, default=True, converter=bool)
 	graph_as_actor = attr.ib(type=bool, default=False, converter=bool)
 	use_vertex_store = attr.ib(type=bool, default=True, converter=bool)
-	vertex_store_as_actor = attr.ib(type=bool, default=True, converter=bool)
+	num_stores = attr.ib(type=int, default=1, converter=int)
+	store_as_actor = attr.ib(type=bool, default=True, converter=bool)
 	detached = attr.ib(type=bool, default=True, converter=bool)
 	_graph = attr.ib(type=FactorGraph, init=False, repr=False)
 	_factors = attr.ib(type=Iterable[Vertex], init=False, repr=False)
 	_variables = attr.ib(type=Iterable[Vertex], init=False, repr=False)
-	_vertex_store = attr.ib(type=FactorGraph, init=False, repr=False)
+	_stores = attr.ib(
+		type=Union[VertexStore, Sequence[VertexStore]], init=False, repr=False)
+	_store_ind = attr.ib(type=int, init=False, repr=False)
 
 	def __attrs_post_init__(self):
+		self._cross_check_graph_state()
 		self._graph = factor_graph_factory(
-			backend=self.backend,
+			impl=self.impl,
 			as_actor=self.graph_as_actor,
 			detached=self.detached)
 		if self.use_vertex_store:
-			self._vertex_store = VertexStore(
-				local_mode=not self.vertex_store_as_actor,
-				detached=self.detached)
+			self._cross_check_num_stores()
+			self._stores = tuple(
+				VertexStore(
+					local_mode=not self.store_as_actor,
+					detached=self.detached)
+				for _ in range(self.num_stores))
+			self._store_ind = 0
 		else:
-			self._vertex_store = None
-		self._factors = set()
-		self._variables = set()
+			self._stores = None
+		n = 1 if self.num_stores == 0 else self.num_stores
+		self._factors = [[] for _ in range(n)]
+		self._variables = [[] for _ in range(n)]
+
+	def _cross_check_num_stores(self):
+		if self.use_vertex_store:
+			num_cpus = backend.NUM_CPUS
+			if self.store_as_actor and self.num_stores > num_cpus:
+				raise ValueError(_NUM_STORES_CPU_EXCEPTION.format(num_cpus))
+			if self.num_stores < 1:
+				raise ValueError(_MIN_NUM_STORES_EXCEPTION)
+
+	def _cross_check_graph_state(self):
+		if self.graph_as_actor and self.share_graph:
+			raise ValueError(_GRAPH_STATE_EXCEPTION)
 
 	def add_variables(
 			self,
-			vertices: Collection[Vertex],
+			vertices: Iterable[Vertex],
 			attributes: Mapping[Vertex, Attributes] = None) -> NoReturn:
-		if self.use_vertex_store:
-			self._graph.add_variables(vertices)
-			self._vertex_store.put(keys=vertices, attributes=attributes)
-		else:
-			self._graph.add_variables(vertices, attributes)
-		self._variables.update(vertices)
+		self._add_vertices(
+			vertices,
+			attributes,
+			self._graph.add_variables,
+			self._variables)
 
 	def add_factors(
 			self,
-			vertices: Collection[Vertex],
+			vertices: Iterable[Vertex],
 			attributes: Mapping[Vertex, Attributes] = None) -> NoReturn:
+		self._add_vertices(
+			vertices,
+			attributes,
+			self._graph.add_factors,
+			self._factors)
+
+	def _add_vertices(self, vertices, attributes, add_func, all_vertices):
 		if self.use_vertex_store:
-			self._graph.add_factors(vertices)
-			self._vertex_store.put(keys=vertices, attributes=attributes)
+			if self.num_stores > 1:
+				for i, v in enumerate(vertices):
+					add_func([v])
+					ind = self._store_ind
+					all_vertices[ind].append((v, ind))
+					attrs = {v: attributes[v]}
+					self._stores[ind].put(keys=[v], attributes=attrs)
+					self._increment()
+			else:
+				self._stores[0].put(keys=vertices, attributes=attributes)
+				all_vertices[0].extend((v, 0) for v in vertices)
 		else:
-			self._graph.add_factors(vertices, attributes)
-		self._factors.update(vertices)
+			add_func(vertices, attributes)
 
 	def add_edges(
 			self,
@@ -425,141 +512,95 @@ class _FactorGraphBuilder:
 			attributes: Mapping[Edge, Attributes] = None) -> NoReturn:
 		self._graph.add_edges(edges, attributes)
 
-	def build(self) -> Union[
-		Tuple[FactorGraph, Iterable[Vertex], Iterable[Vertex], VertexStore],
-		Tuple[FactorGraph, Iterable[Vertex], Iterable[Vertex]],
-		Tuple[FactorGraph, VertexStore],
-		FactorGraph]:
+	def build(self, *, set_graph: bool = False) -> Tuple[
+		FactorGraph,
+		Union[VertexVector, VertexMatrix],
+		Union[VertexVector, VertexMatrix],
+		OptionalVertexStores]:
+		"""Instantiates the built FactorGraph and optional VertexStore.
+
+		If multiple VertexStores are used, the factor and variable vertex
+		lists are list of lists, each list corresponding to the VertexStore
+		in which its attributes are stored.
+
+		Args:
+			set_graph: True will store the factor and variable lists in the
+				Factor Graph instance.
+
+		Returns:
+			FactorGraph instance, factor and variable lists, and optional
+			VertexStore instance.
+
+		"""
+		graph = self._graph
+		factors = self._factors
+		variables = self._variables
+		if set_graph:
+			if self.num_stores > 1:
+				v_ids = itertools.chain.from_iterable(variables)
+				v_ids = (v for v, _ in v_ids)
+				f_ids = itertools.chain.from_iterable(factors)
+				f_ids = (v for v, _ in f_ids)
+			else:
+				v_ids = variables[0]
+				f_ids = factors[0]
+			graph.set_variables(v_ids)
+			graph.set_factors(f_ids)
+			factors = None
+			variables = None
 		if self.share_graph:
-			factors = ray.put(np.array(list(self._factors)))
-			variables = ray.put(np.array(list(self._variables)))
-			graph = ray.put(self._graph)
-			if self.use_vertex_store:
-				handles = (graph, factors, variables, self._vertex_store)
-			else:
-				handles = (graph, factors, variables)
-		else:
-			if self.graph_as_actor:
-				if self.use_vertex_store:
-					handles = (
-						self._graph,
-						self._factors,
-						self._variables,
-						self._vertex_store)
-				else:
-					handles = (self._graph, self._factors, self._variables)
-			else:
-				self._graph.set_factors(self._factors)
-				self._graph.set_variables(self._variables)
-				if self.use_vertex_store:
-					handles = (self._graph, self._vertex_store)
-				else:
-					handles = self._graph
-		return handles
+			graph = ray.put(graph)
+		return graph, factors, variables, self._stores
 
-
-@attr.s(slots=True)
-class FactorGraphBuilder:
-	backend = attr.ib(type=str, default=DEFAULT)
-	as_actor = attr.ib(type=bool, default=True, converter=bool)
-	share_graph = attr.ib(type=bool, default=True, converter=bool)
-	graph_as_actor = attr.ib(type=bool, default=False, converter=bool)
-	use_vertex_store = attr.ib(type=bool, default=True, converter=bool)
-	vertex_store_as_actor = attr.ib(type=bool, default=True, converter=bool)
-	detached = attr.ib(type=bool, default=True, converter=bool)
-	_actor = attr.ib(type=_FactorGraphBuilder, init=False, repr=False)
-
-	def __attrs_post_init__(self):
-		kwargs = {
-			'backend': self.backend,
-			'share_graph': self.share_graph,
-			'graph_as_actor': self.graph_as_actor,
-			'use_vertex_store': self.use_vertex_store,
-			'vertex_store_as_actor': self.vertex_store_as_actor,
-			'detached': self.detached}
-		if self.as_actor:
-			self._actor = ray.remote(_FactorGraphBuilder).remote(**kwargs)
-		else:
-			self._actor = _FactorGraphBuilder(**kwargs)
-
-	def add_variables(self, vertices, attributes=None) -> Optional[Any]:
-		if self.as_actor:
-			value = self._actor.add_variables.remote(vertices, attributes)
-			return ray.get(value)
-		else:
-			self._actor.add_variables(vertices, attributes)
-
-	def add_factors(self, vertices, attributes=None) -> Optional[Any]:
-		if self.as_actor:
-			value = self._actor.add_factors.remote(vertices, attributes)
-			return ray.get(value)
-		else:
-			self._actor.add_factors(vertices, attributes)
-
-	def add_edges(self, edges, attributes=None) -> Optional[Any]:
-		if self.as_actor:
-			value = self._actor.add_edges.remote(edges, attributes)
-			return ray.get(value)
-		else:
-			self._actor.add_edges(edges, attributes)
-
-	def build(self) -> Union[
-		Tuple[FactorGraph, Iterable[Vertex], Iterable[Vertex], VertexStore],
-		Tuple[FactorGraph, Iterable[Vertex], Iterable[Vertex]],
-		Tuple[FactorGraph, VertexStore],
-		FactorGraph]:
-		build = self._actor.build
-		return ray.get(build.remote()) if self.as_actor else build()
-
-	def kill(self):
-		if self.as_actor:
-			ray.kill(self._actor)
+	def _increment(self):
+		reset = (plus_one := self._store_ind + 1) >= self.num_stores
+		self._store_ind = 0 if reset else plus_one
 
 
 def _factor_graph_factory(
-		backend: str, as_actor: bool, detached: bool) -> FactorGraph:
+		impl: str, as_actor: bool, detached: bool) -> FactorGraph:
 	if as_actor:
-		if backend == IGRAPH:
+		if impl == IGRAPH:
 			if detached:
 				graph = ray.remote(IGraphFactorGraph)
 				graph = graph.options(lifetime='detached')
 			else:
 				graph = ray.remote(IGraphFactorGraph)
-		elif backend == NETWORKX:
+		elif impl == NETWORKX:
 			if detached:
 				graph = ray.remote(NetworkXFactorGraph)
 				graph = graph.options(lifetime='detached')
 			else:
 				graph = ray.remote(IGraphFactorGraph)
-		elif backend == NUMPY:
+		elif impl == NUMPY:
 			if detached:
 				graph = ray.remote(NumpyFactorGraph)
 				graph = graph.options(lifetime='detached')
 			else:
 				graph = ray.remote(NumpyFactorGraph)
 		else:
-			raise ValueError(_BACKEND_EXCEPTION)
+			raise ValueError(_IMPL_EXCEPTION)
 		graph = graph.remote()
 	else:
-		if backend == IGRAPH:
+		if impl == IGRAPH:
 			graph = IGraphFactorGraph()
-		elif backend == NETWORKX:
+		elif impl == NETWORKX:
 			graph = NetworkXFactorGraph()
-		elif backend == NUMPY:
+		elif impl == NUMPY:
 			graph = NumpyFactorGraph()
 		else:
-			raise ValueError(_BACKEND_EXCEPTION)
+			raise ValueError(_IMPL_EXCEPTION)
 	return graph
 
 
 def factor_graph_factory(
 		*,
-		backend: str = DEFAULT,
+		impl: str = DEFAULT,
 		as_actor: bool = False,
 		detached: bool = True) -> FactorGraph:
 	if as_actor:
-		graph = RayFactorGraph(backend=backend, detached=detached)
+		graph = RayFactorGraph(impl=impl, detached=detached)
 	else:
 		graph = _factor_graph_factory(
-			backend=backend, as_actor=as_actor, detached=detached)
+			impl=impl, as_actor=as_actor, detached=detached)
 	return graph
