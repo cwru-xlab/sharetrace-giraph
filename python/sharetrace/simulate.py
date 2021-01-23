bc2d8e8b64f1a3f2c8f9322f9a3ebbc951398a0e
Ryan Tatton <rdt17@protonmail.com>
2021-01-23 10:54:29 -0500

    Updated to use renamed modules
    Signed-off-by: Ryan Tatton <rdt17@protonmail.com>

--- python/sharetrace/simulate.py
+++ python/sharetrace/simulate.py
@@ -4,8 +4,8 @@ import random
 import codetiming
 import numpy as np
 
-import _graphs
-import _model
+import graphs
+import model
 import backend
 import propagation
 import search
@@ -25,19 +25,19 @@ def setup(
 	variables = []
 	for u in range(users):
 		risks = [
-			_model.RiskScore(
+			model.RiskScore(
 				name=str(u),
 				timestamp=start + datetime.timedelta(random.randint(0, days)),
 				# Skewed toward lower scores
-				value=random.betavariate(alpha=2, beta=10))
+				value=random.betavariate(alpha=2, beta=5))
 			for _ in range(scores)]
 		variables.append((u, risks))
 		history = (
-			_model.TemporalLocation(
+			model.TemporalLocation(
 				timestamp=start + datetime.timedelta(random.randint(0, days)),
 				location=random.randint(0, locations))
 			for _ in range(locations))
-		factors.append(_model.LocationHistory(name=u, history=history))
+		factors.append(model.LocationHistory(name=u, history=history))
 	return factors, variables
 
 
@@ -48,7 +48,7 @@ def simulate(
 		transmission_rate: float = 0.8,
 		iterations: int = 4,
 		tolerance: float = 1e-5,
-		impl=_graphs.IGRAPH):
+		impl=graphs.IGRAPH):
 	transmission_rate = max((min((1, transmission_rate)), 0))
 	iterations = max((1, iterations))
 	tolerance = max(1e-16, tolerance)
@@ -56,15 +56,15 @@ def simulate(
 		iterations=iterations,
 		transmission_rate=transmission_rate,
 		tolerance=tolerance,
-		backend=impl)
+		impl=impl)
 	return bp(factors=factors, variables=variables)
 
 
 def main():
 	random.seed(12345)
 	np.random.seed(12345)
-	local_mode = True
-	impl = _graphs.NUMPY
+	local_mode = False
+	impl = graphs.NUMPY
 	setup_kwargs = {'users': 200, 'scores': 60, 'days': 30}
 	backend.set_local_mode(local_mode)
 	contact_search = search.ContactSearch()
@@ -80,7 +80,7 @@ def main():
 
 
 if __name__ == '__main__':
-	for i in range(1, 6):
+	for i in range(1, 2):
 		with codetiming.Timer(name='main'):
 			main()
 	print(f"mean:{codetiming.Timer.timers.mean('main')}")
